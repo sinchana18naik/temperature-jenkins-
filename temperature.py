f4fee91b806e2b6b9a91279f576fc56f64eae116
sinchana18naik <sinchananaik112005@gmail.com>
2025-11-27 15:23:48 +0530

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -1,13 +1,13 @@
 import sys
 if len(sys.argv)==2:
   script_name=sys.argv[0]
-  temp=sys.argv[1]
+  temp=int(sys.argv[1])
 else:
   temp=35
 
-if (temp < 15):
+if (temp<15):
   alert="cold"
-elif (temp < 30):
+elif (temp<30):
   alert="normal"
 else:
   alert="hot"
