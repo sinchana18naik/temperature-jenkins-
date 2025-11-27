2248fcd81f5f6d53fd2d6cbd9a83bf67514e1b42
sinchana18naik <sinchananaik112005@gmail.com>
2025-11-27 15:07:37 +0530

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,16 @@
+import sys
+if len(sys.argv)==2:
+  script_name=sys.argv[0]
+  temp=sys.argv[1]
+else:
+  temp=35
+
+if temp <= 15:
+  alert="cold"
+elif temp <=30:
+  alert="normal"
+else:
+  alert="hot"
+
+print("temperature entered is:",temp)
+print("temperature is :",alert)
